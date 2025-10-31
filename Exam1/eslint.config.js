6608b9cc10d17db4ae6c7b4fe8638166050f902f
Jamie Blättler <jamie.blaettler@proton.me>
2025-10-31 12:43:43 +0100

    formatted

--- Exam1/eslint.config.js
+++ Exam1/eslint.config.js
@@ -1,24 +1,15 @@
 // eslint.config.js
 export default [
   {
-    files:
-      [
-        "**/*.js",
-      ],
-    languageOptions:
-      {
-        ecmaVersion: 2022,
-        sourceType:
-          "module",
-      },
-    rules:
-      {
-        "no-unused-vars":
-          "error",
-        "prefer-const":
-          "error",
-        eqeqeq:
-          "error",
-      },
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+    },
+    rules: {
+      "no-unused-vars": "error",
+      "prefer-const": "error",
+      eqeqeq: "error",
+    },
   },
 ];
--- Exam1/index.js
+++ Exam1/index.js
@@ -1,13 +1,7 @@
 import { default as chalk } from "chalk";
 
-console.log(
-  chalk.blue(
-    "It works",
-  ),
-);
+console.log(chalk.blue("It works"));
 
 let myUnusedVariable = 1;
 
-console.log(
-  1 == 1,
-);
+console.log(1 == 1);
